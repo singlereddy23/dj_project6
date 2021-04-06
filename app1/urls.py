bea458c66eb351d2c63c0af77611b388b821dfb5
SingleReddy <singlereddy23@gmail.com>
2021-04-06 10:09:17 +0530

    first commit

--- app1/urls.py
+++ app1/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('sample/',views.sample,name="sample"),
     path("sample_app1/",views.sample_view,name="sample_view"),
+    path("<data>/",views.carry_data,name="carry_data"),
+    path("fact/<num>/",views.facto,name="facto"),
+    path("add/<num1>/<num2>",views.add,name="add"),
 ]
